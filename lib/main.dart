67be6d9a4eab9f1765f85e24c2ab3759dc85e485
JonSnowv2 <dariusandreicoman@gmail.com>
2022-09-15 18:58:51 +0300

    Undid all I did
    Undid all I did cause I dont know what I did

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:buildnow/add_product_page/add_product_page.dart';
 import 'package:buildnow/loginpage/loginpage.dart';
-import 'package:buildnow/profilepagebuyer/profilepagebuyerimproved.dart';
 import 'package:buildnow/register_supplier/supplier_registerpage.dart';
 import 'package:buildnow/registerpage/registerpage_form.dart';
 import 'package:buildnow/productpage/productpage.dart';
